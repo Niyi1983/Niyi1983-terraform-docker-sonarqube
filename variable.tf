a1cde6d860e7cf1f8c2312e6a9a839a58bdc13f0
Niyi1983 <142004482+Niyi1983@users.noreply.github.com>
2023-10-22 10:47:05 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,17 @@
 # Input Variables
 variable "vpc_id" {
   type    = string
-  default = "	vpc-02d9fd54627ca5c19" # Replace with the actual VPC ID of your jenkins svr 
+  default = "	vpc-0a17364a292a1bfd9" # Replace with the actual VPC ID of your jenkins svr 
 }
 
 variable "security_group_id" {
   type    = string
-  default = "sg-004caa499b5e7a54b" # Replace with the actual security group ID of your jenkins sg-group 
+  default = "sg-099edd338288089e6" # Replace with the actual security group ID of your jenkins sg-group 
 }
 
 variable "public_subnet_id" {
   type    = string
-  default = "subnet-0a8a314b70217d378" # Replace with the actual public subnet ID of your jenkins public subnet
+  default = "subnet-07356201e1c32ff12" # Replace with the actual public subnet ID of your jenkins public subnet
 }
 
 variable "aws_region" {
@@ -27,5 +27,5 @@ variable "instance_type" {
 
 variable "instance_keypair" {
   type = string
-  default = "demo-kp"
+  default = "sandbox4"
 }
